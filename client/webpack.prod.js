fcc8f9c9969fb53d705d4087558717fe0bcc98f1
moyui <gxy1510@gmail.com>
2018-02-22 23:54:07 +0800

    20182/22

--- client/webpack.prod.js
+++ client/webpack.prod.js
@@ -1,8 +1,15 @@
 const merge = require('webpack-merge');
+const webpack = require('webpack');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
+  devtool: 'source-map',
+  plugins: [
+    new UglifyJSPlugin({
+      sourceMap: true
+    })
+  ],
   output: {
     filename: '[name].[chunkhash].js',
     path: '../public/dist'
