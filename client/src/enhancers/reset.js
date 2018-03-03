e6d02d769e3178b06f360e70849bfed4e40ef22a
moyui <gxy1510@gmail.com>
2018-03-03 20:17:44 +0800

    2018/3/3

---
+++ client/src/enhancers/reset.js
@@ -0,0 +1,28 @@
+import { createStore } from 'redux';
+
+const RESER_ACTION_TYPE = '@@RESET';
+
+const reserReducerCreator = (reducer, resetState) => (state, aciton) => {
+  if (action.type === RESER_ACTION_TYPE) {
+    return resetState;
+  } else {
+    return reducer(state, action);
+  }
+};
+
+const reset = (createStore) => (reducer, preloadedState, enhancer) => {
+  const store = createStore(reducer, preloadedState, enhancer);
+
+  const reset = (resetReducer, resetState) => {
+    const newReducer = resetReducerCrestor(resetReducer, resetState);
+    store.replaceReducer(newReducer);
+    store.dispatch({type: RESER_ACTION_TYPE, state: resetState});
+  };
+
+  return {
+    ...store,
+    reset
+  };
+};
+
+export default reset;
